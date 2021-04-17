d125c8ee3f8bdfb5f3f590e6485096f1c5647999
eythan <eythan092@gmail.com>
2021-04-17 21:19:33 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
 NUTRITIONIX_HEADERS = {
-    'x-app-id': os.environ.get('x-app-id', '110d0c06'),
-    'x-app-key': os.environ.get('x-app-key', '02457cbe48e42b5050da9898c21ccc06'),
+    'x-app-id': os.environ.get('x-app-id'),
+    'x-app-key': os.environ.get('x-app-key'),
     'x-remote-user-id': '1'
 }
 REQUIRED_NATURAL_ARGUMENTS = ['food-description']
