d589b011af34173c80750b5e8dcb4a5ccaacbd38
eythan <eythan092@gmail.com>
2021-04-17 12:49:44 +0300

    Fixed typo in the app configuration.

--- app_config.py
+++ app_config.py
@@ -2,4 +2,4 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///food-tracker.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///food-tracker.db')
